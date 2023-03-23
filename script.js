a3948c9b78c83d8aa7867b591782ac5a5569b4db
ProstoReals <60000996+ProstoReals@users.noreply.github.com>
2023-03-23 14:11:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function time() {
 var block = document.getElementById(1);
 var block2 = document.getElementById(2);
 
-var thisSecond2 = new Date(2023,0,1);
+var thisSecond2 = new Date(2022,0,1);
 
 console.log("Date - " + thisSecond2);
 today = new Date();
